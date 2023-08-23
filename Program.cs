aa36582a1e121ab3a25cb0ee1d90b14503097ac7
Salma Begum <Salma.Begum@medivet.co.uk>
2023-08-23 23:59:45 +0100

    adding Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PasswordChecker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

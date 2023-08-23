80452031017464a19ec688d49348fa66781fe2f7
Salma Begum <Salma.Begum@medivet.co.uk>
2023-08-24 00:01:05 +0100

    commiting program.cs changes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,43 @@
-﻿namespace PasswordChecker
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace PasswordChecker
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter a password");
+            string password = Console.ReadLine();
+
+            {
+                bool lowercase = true;
+                bool alphanumeric = true;
+                // Check each conditions 
+                foreach (char x in password)
+                {
+                    if (char.IsUpper(x))
+                    {
+                        lowercase = false;
+                    }
+                    else if (!char.IsLetterOrDigit(x))
+                    {
+                        alphanumeric = false;
+                    }
+                }
+                // Check if password meets all conditions
+                bool validPassword = lowercase && alphanumeric;
+
+                if (validPassword)
+                {
+                    Console.WriteLine("This password is valid");
+                }
+                else
+                {
+                    Console.WriteLine("This password is invalid");
+                }
+            }
         }
     }
 }
+
